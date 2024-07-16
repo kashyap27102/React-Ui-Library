52269d29a07251c27f2a41a686ae930c2aceee0f
Kashyap2701 <kspatelsimform100@gmail.com>
2024-07-16 18:46:30 +0530

    feat : Add Button and Input Component

---
+++ src/components/Input/index.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Input } from ".";
+
+const meta: Meta<typeof Input> = {
+  title: "Components/Input",
+  component: Input,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Text: Story = {
+  args: {
+    type: "text",
+    placeholder: "Instert text here",
+  },
+};
+
+export const Password: Story = {
+  args: {
+    type: "password",
+    placeholder: "*******",
+  },
+};
